6738a4ae30a3890ec86edee53ed449136602f694
miguel <miguel.contrerastorrejon@gmail.com>
2018-11-26 16:47:50 -0300

    ADD:ejercicio 4 Resuelto

--- 1 metodos/ejercicio4.rb
+++ 1 metodos/ejercicio4.rb
@@ -1,3 +1,12 @@
 # Crear un método que muestre en pantalla un saludo, el método
 # debe recibir un parámetro, si ese parámetro es el string "Hola" el
 # método debe mostrar en pantalla "Hola Mundo".
+
+def saludo sld
+  if sld == 'hola'
+    puts 'Hola mundo'
+  end
+
+end
+
+puts saludo ('hola')
